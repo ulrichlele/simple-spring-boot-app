0496c5c884ec2b45f5a247b2814d8861465cfe62
Ulrich LELE <lele.ulrich2@gmail.com>
2022-01-31 11:28:59 +0100

    new spring app

---
+++ src/main/java/com/example/sampleboot/SamplebootApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplebootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampleboot/SamplebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
